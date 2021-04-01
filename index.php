a29afbc33f976499e17df4d2b084975c23b01d1c
Ochui Princewill <ochuiprincewill411@gmail.com>
2021-04-01 16:47:46 +0100

    show error

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
-<?php
-error_reporting(0);
-// Version
-define('VERSION', '1.0.0.0');
-
-// Configuration
-if (is_file('config.php')) {
-	require_once('config.php');
-}
-
-// Install
-if (!defined('DIR_APPLICATION')) {
-	header('Location: install/index.php');
-	exit;
-}
-
-// Startup
-require_once(DIR_SYSTEM . 'startup.php');
-
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+// Version
+
+define('VERSION', '1.0.0.0');
+
+
+
+// Configuration
+
+if (is_file('config.php')) {
+
+	require_once('config.php');
+
+}
+
+
+
+// Install
+
+if (!defined('DIR_APPLICATION')) {
+
+	header('Location: install/index.php');
+
+	exit;
+
+}
+
+
+
+// Startup
+
+require_once(DIR_SYSTEM . 'startup.php');
+
+
+
 start('catalog');
